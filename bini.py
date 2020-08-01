6307e902366d3e2cb731a8a53fda51120ac35ba9
Rasmus Bondesson <raek@raek.se>
2020-08-01 23:35:19 +0200

    Simplify BINI parser by reading string table first

--- bini.py
+++ bini.py
@@ -4,7 +4,6 @@ from struct import Struct, pack, unpack
 
 Section = namedtuple("Section", "name, entries")
 Entry = namedtuple("Entry", "key, values")
-Value = namedtuple("Value", "type, data")
 
 HEADER_STRUCT = Struct("<LLL")
 SECTION_STRUCT = Struct("<HH")
@@ -23,34 +22,12 @@ MAGIC = 0x494e4942 # "BINI"
 
 def load(f):
     # Header
-    magic, version, stroff = read_struct(f, HEADER_STRUCT)
+    magic, version, string_table_offset = read_struct(f, HEADER_STRUCT)
     assert magic == MAGIC
     assert version == 1
 
-    # Entries
-    raw_sections = []
-    while f.tell() < stroff:
-        section_index, entry_count = read_struct(f, SECTION_STRUCT)
-        raw_entries = []
-        for _ in range(entry_count):
-            key_index, value_count = read_struct(f, ENRTY_STRUCT)
-            raw_values = []
-            for _ in range(value_count):
-                type_ = read_struct(f, TYPE_STRUCT)[0]
-                if type_ == INTEGER_TYPE:
-                    raw_data = read_struct(f, INTEGER_STRUCT)[0]
-                elif type_ == FLOAT_TYPE:
-                    raw_data = read_struct(f, FLOAT_STRUCT)[0]
-                elif type_ == STRING_TYPE:
-                    raw_data = read_struct(f, STRING_STRUCT)[0]
-                else:
-                    raise ValueError("invalid type %d at offset 0x%x" % (type_, f.tell() - TYPE_STRUCT.size))
-                raw_values.append(Value(type_, raw_data))
-            raw_entries.append(Entry(key_index, raw_values))
-        raw_sections.append(Section(section_index, raw_entries))
-
     # String table
-    assert f.tell() == stroff
+    f.seek(string_table_offset)
     string_table = {}
     string_bytes = f.read()
     current_offset = 0
@@ -61,20 +38,33 @@ def load(f):
         string_table[current_offset] = string
         current_offset = null_offset + 1
 
-    # Replace string indices with string values
+    # Entries
+    f.seek(HEADER_STRUCT.size)
     sections = []
-    for section_index, raw_entries in raw_sections:
+    while f.tell() < string_table_offset:
+        section_index, entry_count = read_struct(f, SECTION_STRUCT)
+        section_name = string_table[section_index]
         entries = []
-        for key_index, raw_values in raw_entries:
+        for _ in range(entry_count):
+            key_index, value_count = read_struct(f, ENRTY_STRUCT)
+            key_name = string_table[key_index]
             values = []
-            for type_, raw_data in raw_values:
-                if type_ == STRING_TYPE:
-                    value = string_table[raw_data]
+            for _ in range(value_count):
+                type_ = read_struct(f, TYPE_STRUCT)[0]
+                if type_ == INTEGER_TYPE:
+                    value = read_struct(f, INTEGER_STRUCT)[0]
+                elif type_ == FLOAT_TYPE:
+                    value = read_struct(f, FLOAT_STRUCT)[0]
+                elif type_ == STRING_TYPE:
+                    string_index = read_struct(f, STRING_STRUCT)[0]
+                    value = string_table[string_index]
                 else:
-                    value = raw_data
+                    raise ValueError("invalid type %d at offset 0x%x" % (type_, f.tell() - TYPE_STRUCT.size))
                 values.append(value)
-            entries.append(Entry(string_table[key_index], values))
-        sections.append(Section(string_table[section_index], entries))
+            entries.append(Entry(key_name, values))
+        sections.append(Section(section_name, entries))
+
+    assert f.tell() == string_table_offset
     return sections
 
 
@@ -82,10 +72,6 @@ def read_struct(f, s):
     bs = f.read(s.size)
     assert len(bs) == s.size
     return s.unpack(bs)
-
-
-def int_to_float(u32):
-    return struct.unpack("<f", struct.pack)
 
 
 if __name__ == "__main__":
